b623ce74d8a0e5de4cc3a56fa891a2ea8cd3a4e1
jp21101326 <jcpizarro@gmail.com>
2025-08-01 19:17:11 -0400

    M4_AE1_ABP-Ejercicio individual - 01-08-2025

--- Modulo_4/M4_AE1_ABP-Ejercicio individual/script.js
+++ Modulo_4/M4_AE1_ABP-Ejercicio individual/script.js
@@ -1,6 +1,7 @@
-
-
+// M4_AE1_ABP-Ejercicio individual [Actividad Evaluada]
+//----------------------------------------------------------
 // 1. Crear la clase Vehiculo
+//----------------------------------------------------------
 class Vehiculo {
     constructor(marca, modelo, año, color, kilometraje) {
         this.marca = marca,
@@ -19,7 +20,9 @@ class Vehiculo {
     }
 }
 
-// 2. Crear clases hijas
+//----------------------------------------------------------
+// 2. Crear clases hijas Auto, Motocicleta y Camión
+//----------------------------------------------------------
 class Auto extends Vehiculo {
     constructor(marca, modelo, año, color, kilometraje, numeroDePuertas) {
         super(marca, modelo, año, color, kilometraje);
@@ -57,26 +60,29 @@ class Camion extends Vehiculo {
         super(marca, modelo, año, color, kilometraje);
         this.capacidadDeCarga = capacidadDeCarga;
     }
-
+    // super.mostrarDetalles
     mostrarDetalles() {
-        console.log("Marca:", this.marca);
-        console.log("Modelo:", this.modelo);
-        console.log("Año:", this.año);
-        console.log("Color:", this.color);
-        console.log("Kilometraje:", this.kilometraje);
+        //console.log("Marca:", this.marca);
+        //console.log("Modelo:", this.modelo);
+        //console.log("Año:", this.año);
+        //console.log("Color:", this.color);
+        //console.log("Kilometraje:", this.kilometraje);
+        super.mostrarDetalles();
         console.log("Capacidad de carga:", this.capacidadDeCarga);
     }
 }
 
-// 3. 
+//----------------------------------------------------------
+// 3. Crear objetos literales
+//----------------------------------------------------------
 let vehiculo = {
     "marca": "Mazda",
     "modelo": "Mazda 3",
     "año": 2025,
     "color": "Negro",
     "kilometraje": 0,
-    
-    mostrarDetalles() {
+
+    mostrarDetalles: function() {
         console.log("Marca:", this.marca);
         console.log("Modelo:", this.modelo);
         console.log("Año:", this.año);
@@ -85,20 +91,25 @@ let vehiculo = {
     }
 }
 
-// 4. 
-let auto1 = new Auto("Suzuki", "Swift", 2025, "Blanco", 0, 4);
-let moto1 = new Motocicleta("Yamaha", "MT-07", 2025, "Negro", 0, "Recto");
-let camion1 = new Camion("Scania", "R500", 2025, "Blanco", 0, 26000);
+//----------------------------------------------------------
+// 4. Crear una instancia de cada clase
+//----------------------------------------------------------
+let auto = new Auto("Suzuki", "Swift", 2025, "Blanco", 0, 4);
+let motocicleta  = new Motocicleta("Yamaha", "MT-07", 2025, "Negro", 0, "Recto");
+let camion  = new Camion("Scania", "R500", 2025, "Blanco", 0, 26000);
 
 
-// 5. 
-let json = {
+//----------------------------------------------------------
+// 5. Generar un archivo JSON
+//----------------------------------------------------------
+let ArchivoJSON = `{
     "vehiculos": [
         {
             "marca": "Suzuki",
             "modelo": "Swift",
             "año": 2025,
             "color": "Negro",
+            "tipo": "auto",
             "kilometraje": 0
         },
         {
@@ -106,6 +117,7 @@ let json = {
             "modelo": "MT-07",
             "año": 2025,
             "color": "Negro",
+            "tipo": "motocicleta",
             "kilometraje": 0
         },
         {
@@ -113,13 +125,37 @@ let json = {
             "modelo": "R500",
             "año": 2025,
             "color": "Blanco",
+            "tipo": "camion",
             "kilometraje": 0
         }
     ]
-}
+}`;
 
-// 6. 
-let jsonJSON = JSON.stringify(json);
-console.log(jsonJSON);
-let jsonJavascript = JSON.parse(jsonJSON);
-console.log(jsonJavascript);
+//----------------------------------------------------------
+// 6. Manipulación de JSON
+//----------------------------------------------------------
+// Convertir a objeto JavaScript usando JSON.parse()
+const objetoVehiculos = JSON.parse(ArchivoJSON);
+// Convertir el objeto de vuelta a cadena JSON con JSON.stringify()
+const CadenaJSON = JSON.stringify(objetoVehiculos);
+
+
+
+//----------------------------------------------------------
+// Mostrar detalles de las instancias
+//----------------------------------------------------------
+console.log("Detalles de las instancias:");
+console.log("\nAuto:");
+auto.mostrarDetalles();
+
+console.log("\nMotocicleta:");
+motocicleta.mostrarDetalles();
+
+console.log("\nCamión:");
+camion.mostrarDetalles();
+
+console.log("\nObjeto Javacript --> usando JSON.parse()");
+console.log(objetoVehiculos);
+
+console.log("\nObjeto de vuelta a una cadena JSON con JSON.stringify()");
+console.log(CadenaJSON);
